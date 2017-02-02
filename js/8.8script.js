c3f0535044cb79b8fb6dca22fc6dc104dcaaf89d
sieniawski <sieniawski7@wp.pl>
2017-02-02 16:31:18 +0100

    task 8.8

---
+++ js/8.8script.js
@@ -0,0 +1,7 @@
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var	newText = 'Zielone słonie';
+var newText = newText.toUpperCase();
+var textRepleced = text.replace('Papugi', newText)
+var textReplacedHalf = textRepleced.substring(0, 45);
+
+alert(textReplacedHalf)
