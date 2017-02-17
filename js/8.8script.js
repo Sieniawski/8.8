37f2e46df63f667a80bf12b0bb61923c300bfb83
Wojciech <sieniawski7@wp.pl>
2017-02-17 12:49:47 +0100

    Update 8.8script.js
    issues done

--- js/8.8script.js
+++ js/8.8script.js
@@ -1,5 +1,5 @@
 var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
-var	newText = 'Zielone słonie';
+var newText = 'Zielone słonie';
 var newText = newText.toUpperCase();
 var textRepleced = text.replace('Papugi', newText)
 var textReplacedHalf = textRepleced.substring(0, 45);
